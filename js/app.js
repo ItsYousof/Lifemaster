9358d44be112b01ec3903decbe31144c7034b28e
Yousof <157152463+ItsYousof@users.noreply.github.com>
2024-06-21 10:45:59 -0400

    Just need more stuff and we're going to be fine.

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Example JavaScript for user interactions
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', function () {
+        alert('Getting Started!');
+    });
+});
